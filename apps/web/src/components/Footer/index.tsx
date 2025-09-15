2e8fc82306535a29154a0d8dd65e456c1f7bc14b
Jhony Moncada <jhony.moncada@gmail.com>
2025-09-15 17:40:07 -0500

    Adding up animations.

--- apps/web/src/components/Footer/index.tsx
+++ apps/web/src/components/Footer/index.tsx
@@ -82,9 +82,19 @@ const Footer = () => {
                   href={href}
                   target="_blank"
                   title={title}
-                  className="inline-flex items-center justify-center rounded-full transition-transform duration-300 hover:-rotate-30"
+                  className="relative inline-flex items-center justify-center
+                             rounded-full overflow-hidden
+                             transition-transform duration-300
+                             hover:-rotate-30
+                             group"
                 >
                   <Image src={src} alt={alt} width={32} height={32} />
+                  <span
+                    className="absolute inset-0
+                               bg-gradient-to-br from-transparent to-secondary-blue
+                               opacity-0 group-hover:opacity-70
+                               transition-opacity duration-300"
+                  />
                 </Link>
               </li>
             ))}
