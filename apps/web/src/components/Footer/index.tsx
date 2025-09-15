a7d32ed13b621c8ca56d5e8d5548f38f93fd42c3
Jhony Moncada <jhony.moncada@gmail.com>
2025-09-15 16:37:34 -0500

    Removing use of tailwind media queries in favor of portrait/landscape.

--- apps/web/src/components/Footer/index.tsx
+++ apps/web/src/components/Footer/index.tsx
@@ -11,20 +11,20 @@ const Footer = () => {
       <Link
         href="/"
         title="Back to home"
-        className="block w-fit md:absolute md:top-14 md:left-1/2 md:-translate-x-1/2"
+        className="block w-fit landscape:absolute landscape:top-14 landscape:left-1/2 landscape:-translate-x-1/2"
       >
         <Image
           src="/assets/images/billionaire-logo.svg"
           width={373}
           height={220}
           alt="Billionaire Logo"
-          className="-ml-2 max-w-[230px] lg:max-w-none"
+          className="-ml-2 max-w-[230px] landscape:max-w-none"
         />
       </Link>
 
-      <div className="md:flex md:justify-between md:mt-14 md:mb-16">
+      <div className="landscape:flex landscape:justify-between landscape:mt-14 landscape:mb-16">
         <nav className="text-accent uppercase font-bold">
-          <ul className="flex flex-col items-end md:items-start">
+          <ul className="flex flex-col items-end landscape:items-start">
             <li>
               <Link href="/" title="Go to home" className="inline-block py-2">
                 Home
@@ -51,8 +51,8 @@ const Footer = () => {
             </li>
           </ul>
         </nav>
-        <nav className="py-8 md:py-0">
-          <ul className="flex gap-x-4 justify-end md:flex-col md:gap-y-6">
+        <nav className="py-8 landscape:py-0">
+          <ul className="flex gap-x-4 justify-end landscape:flex-col landscape:gap-y-6">
             <li>
               <Link href="https://www.tiktok.com/@firefox" target="_blank" title="Visit our TikTok">
                 <Image src="/assets/images/social/tiktok.svg" alt="TikTok" width={32} height={32} />
@@ -90,8 +90,8 @@ const Footer = () => {
         </nav>
       </div>
 
-      <div className="md:flex md:justify-between">
-        <div className="ml-auto text-right mb-20 md:ml-0 md:mb-0 md:text-left">
+      <div className="landscape:flex landscape:justify-between">
+        <div className="ml-auto text-right mb-20 landscape:ml-0 landscape:mb-0 landscape:text-left">
           <p className="mb-4">
             Big Tech wants to own your orbit.
             <br />
@@ -101,10 +101,10 @@ const Footer = () => {
             Build a Billionaire
           </Link>
         </div>
-        <p className="md:self-end">Some kind of messaging goes here. </p>
+        <p className="landscape:self-end">Some kind of messaging goes here. </p>
       </div>
 
-      <div className="flex flex-row justify-between font-medium text-xs10 md:text-base mt-2 pt-2 border-t-2 border-common-ash md:mt-6 md:pt-6">
+      <div className="flex flex-row justify-between font-medium text-xs10 landscape:text-base mt-2 pt-2 border-t-2 border-common-ash landscape:mt-6 landscape:pt-6">
         <span>@{currentYear.getFullYear()} Mozilla. All rights reserved.</span>
         <span>
           Built by{' '}
