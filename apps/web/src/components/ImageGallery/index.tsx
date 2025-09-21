e1a60a1fa4f369ed20becb2f278ac6e5cbc5729c
Jhony Moncada <jhony.moncada@gmail.com>
2025-09-21 09:31:33 -0500

    Update index.tsx

--- apps/web/src/components/ImageGallery/index.tsx
+++ apps/web/src/components/ImageGallery/index.tsx
@@ -11,38 +11,35 @@ export interface ImageGalleryProps {
   }[];
 }
 
-const ImageGallery: FC<ImageGalleryProps> = ({ images }) => {
-  console.log(images.length);
-  return (
-    <section className="mb-4 landscape:mb-8 flex flex-row flex-wrap justify-between gap-y-4 landscape:gap-8">
-      {images.map(({ href, title, alt, src }, index) => {
-        const img = (
-          <Bento
-            image={src}
-            imageAlt=""
-            className="w-[10.5rem] landscape:w-[19rem] border-none aspect-square"
-          />
-        );
-        return (
-          <div key={index} className="relative">
-            {href ? (
-              <Link
-                href={href}
-                target="_blank"
-                title={title}
-                aria-label={alt}
-                className="w-full h-full"
-              >
-                {img}
-              </Link>
-            ) : (
-              img
-            )}
-          </div>
-        );
-      })}
-    </section>
-  );
-};
+const ImageGallery: FC<ImageGalleryProps> = ({ images }) => (
+  <section className="mb-4 landscape:mb-8 flex flex-row flex-wrap justify-between gap-y-4 landscape:gap-8">
+    {images.map(({ href, title, alt, src }, index) => {
+      const img = (
+        <Bento
+          image={src}
+          imageAlt=""
+          className="w-[10.5rem] landscape:w-[19rem] border-none aspect-square"
+        />
+      );
+      return (
+        <div key={index} className="relative">
+          {href ? (
+            <Link
+              href={href}
+              target="_blank"
+              title={title}
+              aria-label={alt}
+              className="w-full h-full"
+            >
+              {img}
+            </Link>
+          ) : (
+            img
+          )}
+        </div>
+      );
+    })}
+  </section>
+);
 
 export default ImageGallery;
