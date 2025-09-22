93820d738315b987970e996d4bc87e645adb0c29
Jhony Moncada <jhony.moncada@gmail.com>
2025-09-21 21:03:04 -0500

    MBBO-88 (#92)
    Adding ability to display videos.
    
    Co-authored-by: Rick Hunter <weps84@gmail.com>

--- apps/web/src/components/ImageGallery/index.tsx
+++ apps/web/src/components/ImageGallery/index.tsx
@@ -8,12 +8,14 @@ export interface ImageGalleryProps {
     src: string;
     href?: string;
     title?: string;
+    isVideo: boolean;
+    videoPosterPath?: string;
   }[];
 }
 
 const ImageGallery: FC<ImageGalleryProps> = ({ images }) => (
   <section className="mb-4 landscape:mb-8 flex flex-row flex-wrap justify-between gap-y-4 landscape:gap-8">
-    {images.map(({ href, title, alt, src }, index) => {
+    {images.map(({ href, title, alt, src, isVideo, videoPosterPath }, index) => {
       const img = (
         <Bento
           image={src}
@@ -21,9 +23,33 @@ const ImageGallery: FC<ImageGalleryProps> = ({ images }) => (
           className="w-[10.5rem] landscape:w-[19rem] border-none aspect-square"
         />
       );
+
+      const video = (
+        <div 
+          className='overflow-hidden w-[10.5rem] landscape:w-[19rem] border-none aspect-square relative'
+        >
+          <video
+            src={src}
+            width={640}
+            height={640}
+            poster={videoPosterPath}
+            muted
+            loop
+            playsInline
+            controls
+            preload='metadata'
+            className='block rounded-[0.75rem]'
+          >
+            Your browser does not support HTML5 video.
+          </video>
+        </div>
+      );
+      
       return (
         <div key={index} className="relative">
-          {href ? (
+          {isVideo ? (
+            video
+          ): (href ? (
             <Link
               href={href}
               target="_blank"
@@ -35,7 +61,8 @@ const ImageGallery: FC<ImageGalleryProps> = ({ images }) => (
             </Link>
           ) : (
             img
-          )}
+          ))}
+
         </div>
       );
     })}
