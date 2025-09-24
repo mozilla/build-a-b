79e137357e74afd6059e4ce9bbdbfbc848795e00
Cristian Etchebarne <cristian.etchebarne@mondorobot.com>
2025-09-23 22:37:58 -0300

    feat: several bug fixes

--- apps/web/src/utils/constants.ts
+++ apps/web/src/utils/constants.ts
@@ -4,7 +4,7 @@ export const BUCKET_NAME = 'billionaires';
 export const COOKIE_NAME = 'user_id';
 export const ID_HISTORY_COOKIE = 'id_history';
 export const DEFAULT_AVATAR_WIDTH = 500;
-export const DEFAULT_AVATAR_HEIGHT = 600;
+export const DEFAULT_AVATAR_HEIGHT = 1200;
 
 export const avatarBentoData: AvatarBentoProps = {
   primaryFlowData: {
