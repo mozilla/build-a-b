b2d1704f05000b50d6c08d0163e0426fd1996430
Tim Spinks <tim@mondorobot.com>
2025-09-30 10:23:52 -0700

    fix: download button error

--- apps/web/src/components/PrimaryFlow/AvatarBentoV2/ActionMenu.tsx
+++ apps/web/src/components/PrimaryFlow/AvatarBentoV2/ActionMenu.tsx
@@ -7,7 +7,7 @@ import { Download } from '@/components/ShareAvatar/Download.svg';
 import { Share } from '@/components/ShareAvatar/Share.svg';
 import { Instagram } from '@/components/ShareAvatar/Instagram.svg';
 import { Threads } from '@/components/ShareAvatar/Threads.svg';
-import { useAvatarDownload, useNavigatorShareAction, useSafeClick, useShareUrls } from '@/hooks';
+import { useAvatarActions, useNavigatorShareAction } from '@/hooks';
 import { AvatarData } from '@/types';
 import clsx from 'clsx';
 import { FC, useMemo, useState } from 'react';
@@ -31,11 +31,14 @@ export type AvatarViewAction = {
 
 const ActionMenu: FC<ActionMenuProps> = ({ avatar, navigatorShareAvailable }) => {
   const [actionType, setActionType] = useState<ActionMenuActionTypeOrNull>(null);
-  const { handleNavigatorShare } = useNavigatorShareAction({ avatar });
-  const { downloadFile, isDownloadReady } = useAvatarDownload({ avatar });
-  const { safeHref } = useShareUrls();
-  const { threadsShareUrl } = useShareUrls();
-  const { preventInvalidClick } = useSafeClick();
+  const {
+    handleNavigatorShare,
+    downloadFile,
+    isDownloadReady,
+    threadsShareUrl,
+    safeHref,
+    preventInvalidClick,
+  } = useAvatarActions({ avatar });
 
   const handleModalClose = (open: boolean) => {
     if (!open) setActionType(null);
@@ -72,7 +75,7 @@ const ActionMenu: FC<ActionMenuProps> = ({ avatar, navigatorShareAvailable }) =>
         },
       },
     }),
-    [],
+    [handleNavigatorShare],
   );
 
   return (
