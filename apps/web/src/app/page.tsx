1ee0ffac26d6636713a844637fd445ae0de0f874
Jhony Moncada <jhony.moncada@gmail.com>
2025-10-04 12:52:43 -0500

    Update page.tsx

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,11 +3,7 @@ import SharedPage from './shared-page';
 import { COOKIE_NAME } from '@/utils/constants';
 import { redirect } from 'next/navigation';
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-}) {
+export default async function Home() {
   const cookieStore = await cookies();
   const { value } = cookieStore.get(COOKIE_NAME) || {};
 
