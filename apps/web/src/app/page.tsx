b5c8c27f30176e1b106f55967830fbadc45b2a78
Jhony Moncada <jhony.moncada@gmail.com>
2025-10-04 13:22:39 -0500

    Keep url parameter.

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,12 +3,19 @@ import SharedPage from './shared-page';
 import { COOKIE_NAME } from '@/utils/constants';
 import { redirect } from 'next/navigation';
 
-export default async function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  const queryParams = await searchParams;
+  const searchTerm = queryParams.s;
+
   const cookieStore = await cookies();
   const { value } = cookieStore.get(COOKIE_NAME) || {};
 
   // If a cookie value exists redirect to the user's avatar page
-  if (value) {
+  if (value && !searchTerm) {
     return redirect(`/a/${value}`);
   }
 
