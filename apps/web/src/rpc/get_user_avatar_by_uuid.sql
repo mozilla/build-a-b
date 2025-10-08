c7056acaeb3094690c5fe4ab7e927af7af4ec0fc
Jhony Moncada <jhony.moncada@gmail.com>
2025-10-08 09:52:32 -0500

    Update get_user_avatar_by_uuid.sql

--- apps/web/src/rpc/get_user_avatar_by_uuid.sql
+++ apps/web/src/rpc/get_user_avatar_by_uuid.sql
@@ -1,7 +1,4 @@
--- Replace existing function (schema-qualified, exact signature)
-DROP FUNCTION IF EXISTS public.get_user_avatar_by_uuid(uuid);
-
-CREATE OR REPLACE FUNCTION public.get_user_avatar_by_uuid(user_uuid UUID)
+CREATE OR REPLACE FUNCTION public.get_user_avatar_by_uuid(user_uuid uuid)
 RETURNS TABLE (
     avatar_id BIGINT,
     asset_riding TEXT,
@@ -12,49 +9,42 @@ RETURNS TABLE (
     selfies JSON
 )
 SET search_path = ''
--- lock down search_path
 LANGUAGE plpgsql AS $func$
 BEGIN
-  RETURN QUERY
-  SELECT
-    a.id AS avatar_id,
-    a.asset_riding,
-    a.asset_instagram,
-    a.character_story,
-    a.first_name,
-    a.last_name,
-    -----------------------------------------------------------------
-    -- Build ordered JSON array of selfies for this user's current avatar
-    -----------------------------------------------------------------
-    COALESCE(
-      (
-        SELECT json_agg(
-                 json_build_object(
-                   'id', s.id,
-                   'asset', s.asset,
-                   'created_at', s.created_at,
-                   'n_index', s.n_index
-                 )
-                 ORDER BY s.n_index ASC
-               )
-        FROM public.selfies AS s
-        WHERE s.avatar_id = a.id
-          AND s.status = 'published'
-      ),
-      '[]'::json
-    ) AS selfies
-  FROM public.users AS u
-  INNER JOIN public.avatars AS a
-    ON u.avatar_id = a.id
-  WHERE u.uuid = user_uuid
-  GROUP BY
-    u.id,
-    a.id,
-    a.asset_riding,
-    a.asset_instagram,
-    a.character_story,
-    a.first_name,
-    a.last_name;
+    RETURN QUERY
+    SELECT
+        a.id AS avatar_id,
+        a.asset_riding,
+        a.asset_instagram,
+        a.character_story,
+        a.first_name,
+        a.last_name,
+        COALESCE(
+            json_agg(
+                json_build_object(
+                    'id', s.id,
+                    'asset', s.asset,
+                    'created_at', s.created_at,
+                    'n_index', s.n_index
+                )
+                ORDER BY s.created_at DESC
+            ) FILTER (WHERE s.id IS NOT NULL),
+            '[]'::json
+        ) AS selfies
+    FROM public.users AS u
+    INNER JOIN public.avatars AS a ON u.avatar_id = a.id
+    LEFT JOIN public.user_selfies AS us ON us.user_id = u.id
+    LEFT JOIN public.selfies AS s ON s.id = us.selfie_id
+    WHERE u.uuid = user_uuid
+      AND (s.status = 'published' OR s.id IS NULL)
+    GROUP BY
+        u.id,
+        a.id,
+        a.asset_riding,
+        a.asset_instagram,
+        a.character_story,
+        a.first_name,
+        a.last_name;
 END;
 $func$;
 
