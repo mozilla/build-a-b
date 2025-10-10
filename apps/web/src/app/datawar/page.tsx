404a7458e817342230b4fc1ad9d7e45f131b52e1
Jhony Moncada <jhony.moncada@gmail.com>
2025-10-10 17:25:25 -0500

    Data War Drops Module.

--- apps/web/src/app/datawar/page.tsx
+++ apps/web/src/app/datawar/page.tsx
@@ -1,15 +1,18 @@
 import { Suspense } from 'react';
+import { avatarBentoData } from '@/utils/constants';
+import { evaluateFlag } from '@/app/flags';
+import { notFound } from 'next/navigation';
 import Image from 'next/image';
 import CountDown from '@/components/CountDown';
 import Hero from '@/components/Hero';
 import LinkButton from '@/components/LinkButton';
 import GetStarted, { type GetStartedProps } from '@/components/PrimaryFlow/GetStarted';
-import { avatarBentoData } from '@/utils/constants';
-import { evaluateFlag } from '@/app/flags';
-import { notFound } from 'next/navigation';
 import CardsSection from '@/components/CardsSection';
 import IconCard from '@/components/IconCard';
 import ImageGallery from '@/components/ImageGallery';
+import Bento from '@/components/Bento';
+import Window from '@/components/Window';
+import PhysicalDeckButton from '@/components/PhysicalDeckButton';
 
 export default async function Page() {
   // Check if DataWar feature is enabled
@@ -156,6 +159,43 @@ export default async function Page() {
       </CardsSection>
 
       <ImageGallery images={imagesForGallery} />
+
+      <section className="mb-4 landscape:mb-8 flex flex-col gap-4 landscape:flex-row landscape:gap-8">
+        <Bento className="border-none h-full landscape:flex-1 landscape:h-auto">
+          <Window className="bg-common-ash">
+            <div className="p-4 landscape:p-12 flex flex-col gap-4">
+              <h2 className="text-title-1 text-charcoal">Dropping at TwitchCon 2025</h2>
+              <p className="text-body-regular text-charcoal text-sm-custom">
+                Can&apos;t make it to TwitchCon? Don’t worry, you can still check out the game,
+                download a printable copy, and share your gameplay and ideas with{' '}
+                <strong>@firefox!</strong>
+              </p>
+              <div className="flex flex-col landscape:flex-row gap-4">
+                <LinkButton
+                  href="https://oqqutatvbdlpumixjiwg.supabase.co/storage/v1/object/public/assets/datawar-full-game.pdf"
+                  title="Get your own cards game"
+                  download
+                  className='secondary-button landscape:w-fit
+                           border-charcoal text-charcoal hover:border-charcoal
+                             hover:bg-charcoal hover:text-common-ash
+                             before:content-[""] before:inline-block before:w-4 before:h-4 before:mr-2
+                             before:bg-current before:mask-[url(/assets/images/icons/download.svg)]
+                             before:mask-no-repeat before:mask-center before:mask-contain'
+                  // trackableEvent="click_download_datawar_deck"
+                >
+                  Download the Deck!
+                </LinkButton>
+                <PhysicalDeckButton />
+              </div>
+            </div>
+          </Window>
+        </Bento>
+        <Bento
+          image="/assets/images/data-war/bag.webp"
+          imageAlt="Billionaire in a box"
+          className="landscape:w-[30%] aspect-[377/275] border-none"
+        />
+      </section>
 
       <CountDown
         targetDate="2025-10-18T10:20:30-07:00"
