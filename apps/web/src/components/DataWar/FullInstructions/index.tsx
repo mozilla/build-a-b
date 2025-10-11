4c29ef4f89ce118d007eb56a48f62d78e94e15d9
Rick Hunter <weps84@gmail.com>
2025-10-11 10:43:34 -0500

    MBBO-242
    Add fixes for the italic words missing

--- apps/web/src/components/DataWar/FullInstructions/index.tsx
+++ apps/web/src/components/DataWar/FullInstructions/index.tsx
@@ -79,7 +79,7 @@ const FullInstructions = () => {
             Chain cards, and apply special effects where instructed (even during War).{' '}
           </p>
           <p className="mt-3">
-            <strong>If a Tracker card is played...</strong>
+            <strong>If a <i>Tracker</i> card is played...</strong>
           </p>
           <ul className="mt-2 mb-2 list-[circle] ml-6">
             <li>
@@ -88,22 +88,22 @@ const FullInstructions = () => {
             </li>
           </ul>
           <p>
-            <strong>If a Blocker card is played (expansion only)...</strong>
+            <strong>If a <i>Blocker</i> card is played (expansion only)...</strong>
           </p>
           <ul className="mt-2 mb-2 list-[circle] ml-6">
-            <li>Play another card and subtract the Blocker value from all opponent plays. </li>
+            <li>Play another card and subtract the <i>Blocker</i> value from all opponent plays. </li>
           </ul>
           <p>
-            <strong>If a Data Grab is played...it&apos;s scramble time!</strong>
+            <strong>If a <i>Data Grab</i> is played...it&apos;s scramble time!</strong>
           </p>
           <ul className="mt-2 mb-2 list-[circle] ml-6">
             <li>
-              All players reach in and grab as many cards as they can from the play area only,
+              All players reach in and grab as many cards as they can <strong>from the play area only,</strong>
               leaving other players&apos; decks and collected Billionaires and Launch Stacks alone.
             </li>
           </ul>
           <p>
-            <strong>If a Launch Stack or Billionaire card is played...</strong>
+            <strong>If a <i>Launch Stack</i> or <i>Billionaire</i> card is played...</strong>
           </p>
           <ul className="mt-2 mb-2 list-[circle] ml-6">
             <li>
