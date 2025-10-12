39b795b4101a5942c085e7a4605a735d3c1530c6
Jhony Moncada <jhony.moncada@gmail.com>
2025-10-12 19:46:50 -0400

    2-space format.

--- apps/web/src/components/DataWar/ListItemCard/index.tsx
+++ apps/web/src/components/DataWar/ListItemCard/index.tsx
@@ -11,18 +11,18 @@ export interface ListItemCardProps {
 const defaultStyles =
   'flex gap-4 p-6 landscape:flex-row landscape:content-center items-center rounded-xl landscape:gap-4 landscape:p-10 bg-gradient-to-r from-[#FFEA80] to-[#FF8A50]';
 
-const ListItemCard:FC<ListItemCardProps> = ({imageAlt, imageSrc, text, className}) => {
-    const classData = clsx(defaultStyles, className);
-    return (
-        <div className={classData}>
-            <div className='block relative landscape:w-[4rem]'>
-                <Image src={imageSrc} alt={imageAlt} width={180} height={180} />
-            </div>
-            <div>
-                <p className='font-extrabold'>{text}</p>
-            </div>
-        </div>
-    );
+const ListItemCard: FC<ListItemCardProps> = ({ imageAlt, imageSrc, text, className }) => {
+  const classData = clsx(defaultStyles, className);
+  return (
+    <div className={classData}>
+      <div className="block relative landscape:w-[4rem]">
+        <Image src={imageSrc} alt={imageAlt} width={180} height={180} />
+      </div>
+      <div>
+        <p className="font-extrabold">{text}</p>
+      </div>
+    </div>
+  );
 };
 
 export default ListItemCard;
