40f24d01eb2ccb44da771798b8449ecc5b8babba
Cristian Etchebarne <cristian.etchebarne@mondorobot.com>
2025-11-05 22:57:00 -0300

    launch stack and deck swapping

--- apps/game/src/providers/GameProvider.tsx
+++ apps/game/src/providers/GameProvider.tsx
@@ -24,7 +24,7 @@ export function GameProvider({ children }: PropsWithChildren) {
 
   // Initialize game on mount
   useEffect(() => {
-    initializeGame('custom', 'custom');
+    initializeGame('random', 'random');
   }, [initializeGame]);
 
   return (
