7932be33b882af4f485e1f131a7525d9bce42522
Jesse Manning <jesse@mondorobot.com>
2025-11-22 03:13:18 -0700

    feat: update next config for web app to set Service-Worker-Allowed header to the root domain for files within public assets game. Using full path the sw.js did not work so this uses a more lenient wildcard which does work and should be fine

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,20 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        // Use a wildcard to catch sw.js reliably
+        source: '/assets/game/:path*',
+        headers: [
+          {
+            key: 'Service-Worker-Allowed',
+            value: '/',
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
