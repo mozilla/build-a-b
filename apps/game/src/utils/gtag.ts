a25fc8afe553e9e3e7f83a69e22c3b88ef7e999b
Jhony Moncada <jhony.moncada@gmail.com>
2025-11-22 10:56:04 -0500

    Analytics base code.

---
+++ apps/game/src/utils/gtag.ts
@@ -0,0 +1,22 @@
+export const gtagEvent = ({
+  action,
+  category,
+  label,
+  value,
+}: {
+  action: string;
+  category?: string;
+  label?: string;
+  value?: number;
+}) => {
+  if (typeof window !== 'undefined' && (window as any).gtag) {
+    (window as any).gtag('event', action, {
+      event_category: category,
+      event_label: label,
+      value,
+    });
+    console.log('Gtag event sent:', { action, category, label, value });
+  } else {
+    console.warn('Gtag not loaded yet');
+  }
+};
